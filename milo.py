d02fa04980a05b20cf5c2f126df7ec2427327467
David DeBoer <ddeboer@berkeley.edu>
2017-12-20 07:10:33 -0800

    tweak to milo

--- milo.py
+++ milo.py
@@ -3,17 +3,10 @@ import code_path
 base_code_path = code_path.set('ProjectData')
 import Data_class
 
+print("Loading milestone class as 'mi'")
 mi = Data_class.Data('milestone')
 mi.readData()
-print('\tmilestone class:  mi')
-
-
-def hera(v):
-    mi.set_state_var(description_length=65)
-    mi.set_state_var(show_color_bar=False)
-    mi.set_state_var(show_cdf=False)
-    mi.find(v, only_late=True, dtype='nsfB')
-    plt.show()
+print("\nCourtesy functions: 'gref', 'hera'")
 
 
 def gref(desc):
@@ -23,3 +16,10 @@ def gref(desc):
     k = mi.getref(desc)
     mi.show(k)
     return k
+
+
+def hera(v):
+    mi.set_state_var(description_length=65)
+    mi.set_state_var(show_color_bar=False)
+    mi.set_state_var(show_cdf=False)
+    mi.find(v, only_late=True, dtype='nsfB')
