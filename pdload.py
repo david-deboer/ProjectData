4d6c0e5f1f985155074b01f71782b0d3c8a16b21
David DeBoer <ddeboer@berkeley.edu>
2018-01-05 15:18:54 -0800

    print tweaking in pdload

--- pdload.py
+++ pdload.py
@@ -7,13 +7,10 @@ import Data_class
 
 mi = Data_class.Data('milestone')
 mi.readData()
-print('\tmilestone array mi')
 ta = Data_class.Data('task', verbose=False)
 ta.readData()
-print('\ttask array ta')
 wb = Data_class.Data('wbs', verbose=False)
 wb.concatDat([mi, ta])
-print('\twbs array wb')
 if False:
     rs = Data_class.Data('reqspec')
     rs.readData()
@@ -31,43 +28,6 @@ if False:
     co.getCost()
     co.getBudget()
     print('\tCost array co')
-
-
-def getlist(filename):
-    try:
-        fp = open(filename, 'r')
-    except IOError:
-        print(filename + "doesn't exist")
-        return 0
-
-    lines = fp.readlines()
-    names = []
-    count = 0
-    for line in lines:
-        count += 1
-        names.append(line.split('|')[0].strip())
-    fp.close()
-    return names
-
-
-def writelist(filename, names):
-    fp = open(filename, 'w')
-    count = 0
-    for n in names:
-        count += 1
-        s = n + '\n'
-        fp.write(s)
-    fp.close()
-    return count
-
-
-def findall(db, field, names=None):
-    if names is None:
-        names = db.data.keys()
-    for v in names:
-        if len(db.data[v][field]) > 0:
-            print(v, db.data[v][field])
-
 
 print("""Read in:
     mi : milestones
