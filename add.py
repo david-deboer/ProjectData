cba44ba1476b3981d234315b94c3025b9969c564
ddeboer <ddeboer@berkeley.edu>
2019-09-09 15:35:48 -0700

    gratituitous add

--- add.py
+++ add.py
@@ -3,10 +3,10 @@
 import argparse
 
 o = argparse.ArgumentParser()
-o.add_argument('name',help='name of addition')
-o.add_argument('as',help='gratuitous as')
-o.add_argument('dbtype',help='allowed data types are milestone, reqspec, risk, interface, task')
-o.add_argument('-s','--show',help='show entries (use defaults)',action='store_true')
+o.add_argument('name', help='name of addition')
+o.add_argument('as', help='gratuitous as')
+o.add_argument('dbtype', help='allowed data types are milestone, reqspec, risk, interface, task')
+o.add_argument('-s', '--show', help='show entries (use defaults)', action='store_true')
 args = o.parse_args()
 dbtype = args.dbtype[0:2].lower()
 dbtypeDict = {'mi':'milestone','re':'reqspec','in':'interface','ri':'risk'}
