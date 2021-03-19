5b5ec7ae9ff7ca64e9f1c987bed38b8b1e96d9ed
David DeBoer <ddeboer@berkeley.edu>
2021-03-19 16:28:51 -0700

    fixed up recurring

--- project_data/pd_tools.py
+++ project_data/pd_tools.py
@@ -3,8 +3,9 @@ from datetime import datetime as dt
 from dateutil import relativedelta
 
 
-def recurring_monthly(start_date, stop_date, base_string):
-    dtime_list = generate_recurring(start_date, stop_date, show_print=False, months=1)
+def recurring_monthly(start_date, stop_date, base_string, tstr=None):
+    dtime_list = generate_recurring(start_date, stop_date, show_print=False,
+                                    tstr_to_try=tstr, months=1)
     time_list = []
     desc_list = []
     for dtentry in dtime_list:
@@ -13,15 +14,32 @@ def recurring_monthly(start_date, stop_date, base_string):
     return time_list, desc_list
 
 
-def generate_recurring(start_date, stop_date, show_print=True, **kwargs):
+def _a_time_conversion(this_date, tstr_list_to_try=None):
+    default_tstr_list_to_try = ['%m/%d/%y', '%m/%d/%Y', '%Y/%m/%d', '%y/%m/%d']
+    if tstr_list_to_try is None:
+        tstr_list_to_try = default_tstr_list_to_try
+    elif isinstance(tstr_list_to_try, str):
+        tstr_list_to_try = [tstr_list_to_try]
+    for this_tstr in tstr_list_to_try:
+        this_tstr = this_tstr.replace('-', '/')
+        try:
+            return dt.strptime(this_date, this_tstr)
+        except ValueError:
+            continue
+    else:
+        raise ValueError('No valid conversion string.')
+
+
+def generate_recurring(start_date, stop_date, show_print=True,
+                       tstr_to_try=None, **kwargs):
     """
-    start_date/stop_date: mm/dd/yy
+    start_date/stop_date in one of formats in tstr_to_try
     kwargs is any interval=value acceptable to dateutil.relativedelta
     """
     if not len(kwargs):
         kwargs['months'] = 3
-    this_date = dt.strptime(start_date, '%m/%d/%y')
-    ending = dt.strptime(stop_date, '%m/%d/%y')
+    this_date = _a_time_conversion(start_date, tstr_to_try)
+    ending = _a_time_conversion(stop_date, tstr_to_try)
     ctr = 0
     data = [this_date]
     while this_date <= ending:
